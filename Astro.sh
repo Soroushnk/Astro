29c696c377dd4ea3551f62fbfab2ad174b05e587
Soroush Nekoozadeh <86857255+Soroushnk@users.noreply.github.com>
2023-08-17 02:52:18 -0400

    Update Astro.sh

--- Astro.sh
+++ Astro.sh
@@ -233,7 +233,14 @@ while true; do
             ;;
         5)
             # Function to add the iptables commands to crontab
-
+            RED='\033[0;31m'
+            GREEN='\033[0;32m'
+            YELLOW='\033[1;33m'
+            BLUE='\033[0;34m'
+            MAGENTA='\033[0;35m'
+            CYAN='\033[0;36m'
+            GRAY='\033[0;37m'
+            NC='\033[0m' # No Color
             while true; do
              clear
             # show options to user
