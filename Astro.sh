ee27803abe7ce673e640e6f5002e52c7b8be5dc9
Soroush Nekoozadeh <86857255+Soroushnk@users.noreply.github.com>
2023-08-26 17:07:24 +0330

    Update Astro.sh
    Fixing the iptable menu

--- Astro.sh
+++ Astro.sh
@@ -233,6 +233,8 @@ while true; do
             ;;
         5)
             # Function to add the iptables commands to crontab
+            
+            while true; do
             RED='\033[0;31m'
             GREEN='\033[0;32m'
             YELLOW='\033[1;33m'
@@ -241,7 +243,6 @@ while true; do
             CYAN='\033[0;36m'
             GRAY='\033[0;37m'
             NC='\033[0m' # No Color
-            while true; do
              clear
             # show options to user
             echo "${YELLOW}1.${NC} ${YELLOW}Install iptables${NC}"
